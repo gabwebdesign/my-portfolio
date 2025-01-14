e1ea1727b0efb4feeee6d44b092ae55add5510f5
gabwebdesign <gab.webdesign@gmail.com>
2025-01-14 16:42:38 -0600

    paginas seundarias

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import Experience from "./components/experience/experience";
 import Contact from "./components/contact/contact";
 import Introducing from "./components/introducing/introducing";
 import Main from "./components/main/main";
+import Services from "./components/services/services";
 
 export default function Home() {
   return (
     <div className="grid wrapper overflow-hidden" id="container">
       <Main />
+      <Services />
       <Introducing />
       <Skills />
       <Experience />
