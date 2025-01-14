e7c940cd188e593e64978693bdea407f1f412eab
gabwebdesign <gab.webdesign@gmail.com>
2025-01-13 22:53:10 -0600

    pagin main nuevo diseno

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Skills from "./components/skills/skills";
 import Experience from "./components/experience/experience";
 import Contact from "./components/contact/contact";
 import Introducing from "./components/introducing/introducing";
+import Main from "./components/main/main";
 
 export default function Home() {
   return (
     <div className="grid wrapper overflow-hidden" id="container">
+      <Main />
       <Introducing />
       <Skills />
       <Experience />
