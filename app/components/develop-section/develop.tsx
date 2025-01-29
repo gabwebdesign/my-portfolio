5a1d66e69d2ba2dcce8604d6acc459172f69e466
gabwebdesign <gab.webdesign@gmail.com>
2025-01-29 17:14:48 -0600

    contact section

--- app/components/develop-section/develop.tsx
+++ app/components/develop-section/develop.tsx
@@ -8,13 +8,13 @@ export default function Develop() {
             icon: '/images/icons/layout.png',
             title: 'Frontend',
             description: 'I will develop a flow to guide the user to driven the info desired by the company',
-            className: 'conexion-left row-start-1 row-end-6'
+            className: 'conexion-1 row-start-1 row-end-6'
         },
         {
             icon: '/images/icons/waiter.png',
             title: 'Backend',
             description: 'I will develop a flow to guide the user to driven the info desired by the company',
-            className: 'conexion-left row-start-3 row-end-11'
+            className: 'conexion-2 row-start-3 row-end-11'
         },
         {
             icon: '/images/icons/database.png',
@@ -26,25 +26,45 @@ export default function Develop() {
 
     const works:WorkProps[] = [
         {
-            icon: '/images/icons/meal.png',
-            title: 'Whatsapp Chatbot Restaurant',
+            icon: '/images/icons/quierolote-icon.png',
+            title: 'Quierolote Platform',
             className: 'bg-orange'
         },
         {
-            icon: '/images/icons/robot.png',
-            title: 'IA Agent',
+            icon: '/images/icons/havas.png',
+            title: 'Havas Life Chicago',
+            className: 'bg-orange'
+        },
+        {
+            icon: '/images/icons/havas.png',
+            title: 'Karyforward Website',
+            className: 'bg-orange'
+        },
+        {
+            icon: '/images/icons/havas.png',
+            title: 'Havas Life New York',
+            className: 'bg-orange'
+        },
+        {
+            icon: '/images/icons/havas.png',
+            title: 'Xpovio',
+            className: 'bg-orange'
+        },
+        {
+            icon: '/images/icons/angelina.png',
+            title: 'Café Angelina',
             className: 'bg-orange'
         }
     ]
 
     return (
         <section className="mt-5">
-            <div className="mb-28 flex flex-col lg:flex-row gap-6">
+            <div className="mb-18 flex flex-col lg:flex-row gap-6">
                 <div className="lg:w-96">
                     <h1 className="mb-12">Are you ready to < br className="hidden md:block"/> <span className="text-orange">&lt;open source of success&gt;</span></h1>
                 </div>
                 <div>
-                    <div className="phases w-full gap-3 mb-28">
+                    <div className="phases w-full mb-28">
                         {services.map((service, index) => (
                             <CardServices key={index} {...service} />
                         ))} 
@@ -59,7 +79,7 @@ export default function Develop() {
                 </p>
             </div>
 
-            <div className="w-full flex flex-col md:flex-row gap-12 md:gap-3 mb-20">
+            <div className="w-full flex flex-col md:flex-row gap-12 md:gap-3 mb-20 flex-wrap">
                 
                 <h4 className="mb-6 p-12 text-center">Work</h4>
 
