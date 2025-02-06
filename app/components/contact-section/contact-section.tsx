53b454c91c55596da9c5527d3deb394c7a50d735
gabwebdesign <gab.webdesign@gmail.com>
2025-02-05 23:48:47 -0600

    missing var not used

--- app/components/contact-section/contact-section.tsx
+++ app/components/contact-section/contact-section.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './contact-section.css';
 import Image from 'next/image';
 import { createOportunity } from '../utils/actions';
